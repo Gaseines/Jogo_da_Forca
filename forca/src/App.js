348e8038b2e6e599492be19284f8c397272514c7
Gaseines <gaseinesdzn@hotmail.com>
2024-11-20 23:35:24 -0300

    Parte do game finalizada
    Tela do game finalizada, falta apenas a tela de vitórioa, redirecionamento para derrota feita e forca tambem

--- forca/src/App.js
+++ forca/src/App.js
@@ -13,51 +13,107 @@ import Game from "./components/Game";
 import End from "./components/End";
 
 function App() {
-  const [stage, setStage] = useState("start")
+
+  let qtdTentativas = 7
+
+  const [stage, setStage] = useState("start");
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+  const [guessedLetters, setGuessedLetters] = useState([]);
+  const [errLetters, setErrLetters] = useState([]);
+  const [guesses, setGuesses] = useState(qtdTentativas);
+  const [score, setScore] = useState(0);
 
   const [words] = useState(wordsList);
 
   //Pick a category and word
-  const PickCategoryAndWord = () =>{
+  const PickCategoryAndWord = () => {
     //Pick categories
-    const categories = Object.keys(words)
+    const categories = Object.keys(words);
     //Pick a random category
-    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)]
+    const category =
+      categories[Math.floor(Math.random() * Object.keys(categories).length)];
 
     //Pick a random word
-    const word = words[category][Math.floor(Math.random() * Object.keys(category).length)]
+    const word =
+      words[category][Math.floor(Math.random() * Object.keys(category).length)];
 
-    return {word, category}
-  }
+    return { word, category };
+  };
 
   //Start the game
-  const StartGame = () =>{
+  const StartGame = () => {
     //Select random category
-   const { word, category} = PickCategoryAndWord()
-   
+    const { word, category } = PickCategoryAndWord();
 
-   //Separando em Letras
-   let letters = word.split("")
-   //Letras minusculas
-   letters = letters.map((l) => l.toLowerCase())
+    //Separando em Letras
+    let wordLetters = word.split("");
+    //Letras minusculas
+    wordLetters = wordLetters.map((l) => l.toUpperCase());
 
-   console.log(word, category, letters)
+    console.log(word, category, wordLetters);
+    setPickedCategory(category);
+    setPickedWord(word);
+    setLetters(wordLetters);
+    setStage("game");
+  };
 
-    setStage("game")
+  const VerifyLetter = (letter) => {
+
+    //Inclui as letras certas e erradas ao array
+    if (letters.includes(letter)) {
+      setGuessedLetters((actualGuessedLetters) => [
+        ...actualGuessedLetters,
+        letter,
+      ]);
+    } else {
+      setErrLetters((actualErrLetterss) => [...actualErrLetterss, letter]);
+
+      //Diminuui 1 nas chances do usuario
+      setGuesses(guesses - 1)
+    }
+
+  };
+// função para limpar os dados antigos
+  function clearLetterStates(){
+    setErrLetters([])
+    setGuessedLetters([])
   }
 
-  const GameOver = () =>{
-    setStage("end")
-  }
+  useEffect(() => {
+    if(guesses === 0){
+      //Muda o stage da pagina para o fim de jogo
+      setStage("end")
 
-  const Restart = () =>{
-    setStage("start")
-  }
+      //Limpa os dados antigos
+      clearLetterStates()
+    }
+  }, [guesses])
+
+  console.log(guesses);
+
+  const Restart = () => {
+    setStage("start");
+    setScore(0)
+    setGuesses([qtdTentativas])
+  };
 
   return (
     <div className="App">
       {stage === "start" && <StartView StartGame={StartGame} />}
-      {stage === "game" && <Game GameOver={GameOver} />}
+      {stage === "game" && (
+        <Game
+          VerifyLetter={VerifyLetter}
+          pickedCategory={pickedCategory}
+          pickedWord={pickedWord}
+          letters={letters}
+          guessedLetters={guessedLetters}
+          errLetters={errLetters}
+          guesses={guesses}
+          score={score}
+        />
+      )}
       {stage === "end" && <End Restart={Restart} />}
     </div>
   );
--- forca/src/components/Game.js
+++ forca/src/components/Game.js
@@ -1,35 +1,120 @@
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./Game.module.css";
 import Button from "./subComponents/Button";
 
-const Game = ({ GameOver }) => {
+const Game = ({
+  VerifyLetter,
+  pickedCategory,
+  pickedWord,
+  letters,
+  guessedLetters,
+  errLetters,
+  guesses,
+  score,
+}) => {
+
+  const headRef = useRef(null)
+  const bodyRef = useRef(null)
+  const arm1Ref = useRef(null)
+  const arm2Ref = useRef(null)
+  const leg1Ref = useRef(null)
+  const leg2Ref = useRef(null)
+
+  const [letter, setLetter] = useState("")
+  const inputRef = useRef(null)
+
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    const l = letter.toUpperCase()
+    VerifyLetter(l)
+    setLetter("")
+
+    inputRef.current.focus()
+  }
+
+  useEffect(() => {
+    if(guesses === 6){
+      headRef.current.style.display = "block"
+    }
+    if(guesses === 5){
+      bodyRef.current.style.display = "block"
+    }
+    if(guesses === 4){
+      arm1Ref.current.style.display = "block"
+    }
+    if(guesses === 3){
+      arm2Ref.current.style.display = "block"
+    }
+    if(guesses === 2){
+      leg1Ref.current.style.display = "block"
+    }
+    if(guesses === 1){
+      leg2Ref.current.style.display = "block"
+    }
+  }, [guesses])
+
   return (
     <div className={`container ${styles.game}`}>
       <div className={styles.container_title_and_man}>
         <div className={styles.view}>
-          <div className="points">
-            Sua pontuação: <span>000</span>
+          <div className={styles.points}>
+            Sua pontuação: <span>{score}</span>
           </div>
           <h1>Jogo da Forca</h1>
-          <div className="tip">
-            Categoria: <span>Dica</span>
+          <div className={styles.tip}>
+            Categoria: <span>{pickedCategory}</span>
           </div>
         </div>
         <div className={styles.stick_man}>
-        <div className={styles.forc}>
-          <div className={styles.t1}></div>
-          <div className={styles.t2}></div>
-          <div className={styles.t3}></div>
-          <div className={styles.t4}></div>
-          <div className={styles.head}></div>
-          <div className={styles.body}></div>
-          <div className={styles.arm01}></div>
-          <div className={styles.arm02}></div>
-          <div className={styles.leg01}></div>
-          <div className={styles.leg02}></div>
-        </div>
+          <div className={styles.forc}>
+            <div className={styles.t1}></div>
+            <div className={styles.t2}></div>
+            <div className={styles.t3}></div>
+            <div className={styles.t4}></div>
+            <div ref={headRef} className={styles.head}></div>
+            <div ref={bodyRef} className={styles.body}></div>
+            <div ref={arm1Ref} className={styles.arm01}></div>
+            <div ref={arm2Ref} className={styles.arm02}></div>
+            <div ref={leg1Ref} className={styles.leg01}></div>
+            <div ref={leg2Ref} className={styles.leg02}></div>
+          </div>
         </div>
       </div>
-      <Button text={"Enviar"} func={GameOver} />
+      <div className={styles.letters}>
+        {letters.map((letter, i) =>
+          guessedLetters.includes(letter) ? (
+            <span key={i} className={styles.letter}>
+              {letter}
+            </span>
+          ) : (
+            <span className={styles.box_blank}></span>
+          )
+          
+        )}
+      </div>
+
+      <div className={styles.letters_send}>
+        <p>Letras já utilizadas:</p>
+        {errLetters.map((letter, i) => (
+          <span key={i}>{letter},</span>
+        ))}
+      </div>
+
+      <div className={styles.container_form}>
+        <p>Tente adivinha uma letra da palavra: </p>
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="letter"
+            maxLength={"1"}
+            required
+            onChange={(e) => setLetter(e.target.value)}
+            value={letter}
+            ref={inputRef}
+          />
+          <Button text={"Enviar"} />
+        </form>
+      </div>
     </div>
   );
 };
